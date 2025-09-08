536a18ea028b4c56b239b3e723458352f338a2d3
Taras0310 <hoverla990310@gmail.com>
2025-09-08 18:03:41 +0300

    Tasks 1-3

---
+++ Task1.rb
@@ -0,0 +1,44 @@
+=begin
+Create class which contain instance class and global variable
+Create methods in class which do manipulations(puts, calculates something based on it etc) with eatch type of variable: local, instance, class, global
+Create method in class which would use string interpolation(this can be simple puts one of variable)  
+=end
+
+
+$global = "Human" #global variable
+
+
+class Person
+  
+  attr_accessor :name, :age, :surname, :patronymic
+  @@count_instances = 0 #class variable
+  
+   def initialize(name, age, surname, patronymic) # all this variables are instance variables
+     @name = name
+     @age = age
+     @surname = surname
+     @patronymic = patronymic
+     @@count_instances +=1
+   end
+   
+   def self.count_instances
+    @@count_instances
+  end
+  
+  def set_fulname
+    fulname = "#{@surname} #{@name} #{@patronymic}"
+    
+    fulname
+  end
+  
+  def general_info # in this method all types of variables are used 
+    full = set_fulname #full is the local variable which used only in this method
+    puts " Hi, I'm a #{$global}.\n My fulname is #{full}.\n My age is #{age}.\n All information about me is described in the #{self.class} class.\n There curently #{Person.count_instances} instances of the class.\n"
+  end
+end
+
+i = Person.new("Tарас", 25, "Якушевич", "Олексійович")
+
+
+
+i.general_info
---
+++ Task2.rb
@@ -0,0 +1,49 @@
+=begin
+Create class Vehicle
+Vehicle has year model owner and manufacturer
+Class should have the following attributes 
+- attr accessor owner
+- attr reader: year, model, manufacturer
+- class attribute: count of wheels which is accessible to all class instance
+
+Create three instances of Vehicle class:
+-Audi A3, 1979, owmer - Antonio
+- Moskvych 412, 988, not owner at the moment of Vehicle creation. After set owner as Vlad
+- Skoda Fabia 1990, owner - Tony
+=end
+
+class Vehicle
+  
+  attr_accessor :owner
+  attr_reader :year, :model
+  @@count_wheels = 4
+  
+  def initialize(year, model, owner = nil)
+    @year = year
+    @model = model
+    @owner = owner
+  end
+  
+  def self.count_wheels
+    @@count_wheels
+  end
+  
+  def count_wheels
+    self.class.count_wheels
+  end
+  
+end
+
+instance_1 = Vehicle.new("Audi A3", 1979, "Antonio")
+
+instance_2 = Vehicle.new("Moskvych 412", 988)
+instance_2.owner = "Vlad"
+
+instance_3 = Vehicle.new("Skoda Fabia", 1990, "Tony")
+
+
+
+
+
+
+
